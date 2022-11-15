264f043e64f6a6bb939d51ad325ffe7b89c80c15
HeineTL <116063325+HeineTL@users.noreply.github.com>
2022-11-15 14:19:51 +0100

    Lagt til profil side og fikset pokedex

--- script.js
+++ script.js
@@ -1,25 +1,113 @@
 //MODEL
 const view = document.getElementById("insert-view");
 
-const pokemon =
+const pokedex =
     [
         {
             pokemons: [
                 {
                     name: "Bulbasaur",
-                    type: ["grass", "fire", "poison"],
-                    abilities: "Fireball",
-                    category: "Flame",
-                    height: "160cm",
-                    weight: "200kg"
+                    type: ["Grass", "Poison"],
+                    abilities: "Overgrow",
+                    category: "Seed",
+                    height: "61cm",
+                    weight: "6,8kg",
                 },
                 {
                     name: "Charmander",
-                    type: ["fire"],
+                    type: ["Fire"],
                     abilities: "Blaze",
                     category: "Lizard",
                     height: "60cm",
-                    weight: "8,6kg"
+                    weight: "8,5kg",
+                },
+                {
+                    name: "Squirtle",
+                    type: ["Water"],
+                    abilities: "Torrent",
+                    category: "Tiny Turtle",
+                    height: "33cm",
+                    weight: "8,9kg",
+                },
+                {
+                    name: "Weedle",
+                    type: ["Poison"],
+                    abilities: "Shield Dust",
+                    category: "Hairy Bug",
+                    height: "30cm",
+                    weight: "3,2kg",
+                },
+                {
+                    name: "Pikachu",
+                    type: ["Electric"],
+                    abilities: "Static",
+                    category: "Mouse",
+                    height: "31cm",
+                    weight: "6kg",
+                },
+                {
+                    name: "Jigglypuff",
+                    type: ["Fairy"],
+                    abilities: "Cute Charm",
+                    category: "Balloon",
+                    height: "32cm",
+                    weight: "5,4kg",
+                },
+                {
+                    name: "Meowth",
+                    type: ["Normal"],
+                    abilities: "Pickup",
+                    category: "Scratch Cat",
+                    height: "31cm",
+                    weight: "4,2kg",
+                },
+                {
+                    name: "Psyduck",
+                    type: ["Water"],
+                    abilities: "Cloud Nine",
+                    category: "Duck",
+                    height: "63cm",
+                    weight: "19,5kg",
+                },
+                {
+                    name: "Abra",
+                    type: ["Psychic"],
+                    abilities: "Inner Focus",
+                    category: "Psi",
+                    height: "65cm",
+                    weight: "18,5kg",
+                },
+                {
+                    name: "Golem",
+                    type: ["Rock"],
+                    abilities: "Rock Head",
+                    category: "Megaton",
+                    height: "124cm",
+                    weight: "300kg",
+                },
+                {
+                    name: "Slowpoke",
+                    type: ["Water", "Psychic"],
+                    abilities: "Own Tempo",
+                    category: "Dopey",
+                    height: "94cm",
+                    weight: "36kg",
+                },
+                {
+                    name: "Magnemite",
+                    type: ["Electric"],
+                    abilities: "Magnet Pull",
+                    category: "Magnet",
+                    height: "30,4cm",
+                    weight: "6kg",
+                },
+                {
+                    name: "Bellsprout",
+                    type: ["Grass", "Poison"],
+                    abilities: "Chlorophyll",
+                    category: "Flower",
+                    height: "62cm",
+                    weight: "4kg",
                 },
             ],
         }
@@ -29,58 +117,30 @@ const pokemon =
 pokedexView();
 
 function pokedexView() {
-    // view.innerHTML = /*HTML*/`
-    //     <div class="card-list">
-    //         <div class="cards">
-    //             <div class="pokemon-image">
-    //                 <img src="img/Bulbasaur.png" />
-    //             </div>
-    //             <h1>Bulbasaur</h1>
-    //             <div class="type">
-    //                 <span class="grass">Grass</span>
-    //                 <span class="fire">Fire</span>
-    //                 <span class="water">Water</span>
-    //                 <span class="poison">Poison</span>
-    //             </div>
-    //         </div>
-    //     </div>
-    // `;
-    generatePokedexView();
+
+    view.innerHTML = /*HTML*/`
+    <div class='card-list'>
+        ${generatePokedexCards()}
+    </div>
+    `;
+
 }
 
-function profileView() {
+function profileView(name, index) {
     view.innerHTML = /*HTML*/`
         <div class="card-list">
             <div class="cards profile">
                 <div class="pokemon-image">
-                    <img src="img/Bulbasaur.png" />
+                    ${addImage(name)}
                 </div>
-                <h1>Bulbasaur</h1>
+                <h1>${name}</h1>
                 <div class="type">
-                    <span class="grass">Grass</span>
-                    <span class="fire">Fire</span>
-                    <span class="water">Water</span>
-                    <span class="poison">Poison</span>
+                    ${addTypes(index)}
                 </div>
                 <div class="pokemon-info">
-                    <div class="pokemon-info-card">
-                        <h2>Abilities</h2>
-                        <p>Fireball</p>
-                    </div>
-                    <div class="pokemon-info-card">
-                        <h2>Category</h2>
-                        <p>Flame</p>
-                    </div>
-                    <div class="pokemon-info-card">
-                        <h2>Height</h2>
-                        <p>160cm</p>
-                    </div>
-                    <div class="pokemon-info-card">
-                        <h2>Weight</h2>
-                        <p>200kg</p>
-                    </div>
+                    ${generatePokemonInfo(index)}
                 </div>
-                <button>Go back</button>
+                <button onclick="pokedexView()">Go back</button>
             </div>
         </div>
     `;
@@ -113,25 +173,72 @@ function addView() {
 }
 
 //CONTROLLER
-function generatePokedexView() {
-    for (let i = 0; i < pokemon[0].pokemons.length; i++) {
-        let name = pokemon[0].pokemons[i].name;
+function generatePokedexCards() {
+    let content = "";
 
-        view.innerHTML += /*HTML*/`
-            <div class="card-list">
-                <div class="cards">
-                    <div class="pokemon-image">
-                        <img src="img/${name}.png" onerror="this.style.display = 'none'"/>
-                    </div>
-                    <h1>${name}</h1>
-                    <div class="type">
-                        <span class="grass">Grass</span>
-                        <span class="fire">Fire</span>
-                        <span class="water">Water</span>
-                        <span class="poison">Poison</span>
-                    </div>
+    for (let i = 0; i < pokedex[0].pokemons.length; i++) {
+        let name = pokedex[0].pokemons[i].name;
+
+        content += /*HTML*/`
+            <div class="cards" onclick="profileView('${name}', '${i}')">
+                <div class="pokemon-image">
+                    ${addImage(name)}
                 </div>
+                <h1>${name}</h1>
+                ${addTypes(i)}
             </div>
-    `;   
+    `;
     }
+
+    return content;
+}
+
+function generatePokemonInfo(index) {
+    let text = "";
+
+    let questionsAmount = Object.keys(pokedex[0].pokemons[index]).length;
+
+    let questions = Object.keys(pokedex[0].pokemons[index]);
+
+
+    for (let i = 2; i < questionsAmount; i++) {
+
+        let ttt = questions[i];
+
+        text += /*HTML*/`
+        <div class="pokemon-info-card">
+            <h2>${questions[i].toUpperCase()}</h2>
+            <p>${pokedex[0].pokemons[index][questions[i]]}</p>
+        </div>
+        `;
+    }
+
+    return text;
+}
+
+function addImage(imageSrc) {
+    let http = new XMLHttpRequest();
+
+    let img = "img/" + imageSrc + ".png";
+
+    http.open('HEAD', img, false);
+    http.send();
+
+    let svar = http.status != 404 ? `<img src="${img}">` : `<img src="img/Pokeball.png">`;
+
+    return svar;
+}
+
+function addTypes(index) {
+    let types = pokedex[0].pokemons[index].type;
+
+    let type = "<div class='type'>";
+
+    for (let i = 0; i < types.length; i++) {
+        type += `<span class="${types[i].toLowerCase()}">${types[i]}</span>`;
+    }
+
+    type += "</div>";
+
+    return type;
 }
